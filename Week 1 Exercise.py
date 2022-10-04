5bf727ea0b6344b6c43960359f5a7f72107faecb
Gvalencia2 <115028134+Gvalencia2@users.noreply.github.com>
2022-10-04 11:25:43 -0500

    Add files via upload

---
+++ Week 1 Exercise.py
@@ -0,0 +1,10 @@
+# This is my first hello world program
+# Author: Gabriel
+# Date: 10/4/22
+# Week 1 Lab
+# Purpose: Print hello world
+
+month = "October"
+day = "Tuesday"
+
+print("Today is", day, month,"4th, 2022!") # Output the message to the screen
---
+++ Week 1 First Program.py
@@ -0,0 +1,13 @@
+# This is my first hello world program
+# Author: Gabriel
+# Date: 10/4/22
+# Week 1 Lab
+# Purpose: Print hello world
+
+
+print("Hello World!")
+print(1+1)
+
+name = "Gabriel"
+
+print("Hello",name,"Welcome To My Game!",sep=', ')
